07a9514ca59e65760b459354bc3db700b5782430
kclown0 <keumdy0@gmail.com>
2022-08-22 18:32:26 +0900

    controller

---
+++ src/main/java/githubtest/Controller.java
@@ -0,0 +1,12 @@
+package githubtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application !";
+    }
+}
